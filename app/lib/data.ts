5361f652a184942839de6c87c7aed2563b191332
Won Park <parkwon@sysnova.co.kr>
2024-02-07 17:16:22 +0900

    08 static and dynamic rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,22 +9,24 @@ import {
 } from './definitions';
 
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 import { sql } from '@vercel/postgres';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
